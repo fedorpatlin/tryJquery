1167d66b4627f2602db4e7e87894f1a936110e03
Fedor Patlin <fedorpatlin@gmail.com>
2012-05-03 18:53:40 +0600

    new file:   .project 	new file:   css/slider.css 	new file:   flip-slideshow.js 	new file:   img/gimage_730.jpg 	new file:   img/picoUPS-100-big2.jpg 	new file:   index.html 	new file:   jquery-1.2.6.min.js

---
+++ flip-slideshow.js
@@ -0,0 +1,24 @@
+(function($){
+    $.fn.jMask = function(options,sliderImages){
+        var defaults={time:7000, width:900, height:400, bg:'#ffffff',border:'1px solid #ffffff',callback:function(){  } };
+        var options=$.extend(defaults, options);
+        
+        var root=$(this); 
+        root.html("<ul id=\'slideShow\'></ul>");
+        var ul=root.find("ul:first");
+        setImages(sliderImages);
+        
+        
+        
+        function setImages(jsonArray){
+        	for (i=0; i<jsonArray.length; i++) {
+        		var image = JSON.parse(jsonArray[i]);
+				ul.html(ul.html()+"<li id="+"slide-"+i+"><img class=\"slides\" src="+image.url+ "></img></li>")        		
+        	}
+        }
+        return root;
+    }
+    
+    
+})(jQuery);
+
