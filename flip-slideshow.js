d15eff9bf74b3e50227843a013cb747167e19771
Fedor Patlin <fedorpatlin@gmail.com>
2012-05-04 17:57:49 +0600

    modified:   css/slider.css 	modified:   flip-slideshow.js 	modified:   index.html

--- flip-slideshow.js
+++ flip-slideshow.js
@@ -1,25 +1,52 @@
-(function($){
-    $.fn.jMask = function(options,sliderImages){
-        var defaults={time:7000, width:900, height:400, bg:'#ffffff',border:'1px solid #ffffff',callback:function(){  } };
-        var options=$.extend(defaults, options);
-        
-        var root=$(this); 
-        root.html("<ul id=\'slideShow\'></ul>");
-        var ul=root.find("ul:first");
-        setImages(sliderImages);
-        
-        
-        
-        function setImages(jsonArray){
-        	for (i=0; i<jsonArray.length; i++) {
-        		var image = JSON.parse(jsonArray[i]);
-				ul.html(ul.html()+"<li id="+"slide-"+i+"><img class=\"slides\" src="+image.url+ "></img></li>")        		
-        	}
-        }
-        root.slides.css()
-        return root;
-    }
-    
-    
+(function($) {
+
+	var OPTIONS, DEFAULTS = null;
+	var LIST_ARRAY = new Array();
+
+	var methods = {
+		init : function(options) {
+			root = $(this);
+			initdefaults();
+			OPTIONS = $.extend(DEFAULTS, options);
+			root.html("<ul id=\'slideShow\'></ul>");
+			var ul = root.find("ul:first");
+			setImages(ul, options.sliderImages);
+			root.find("li:first").addClass('top');
+			root.find("li:last").addClass('rest');
+		}
+	}
+
+	function initdefaults() {
+		DEFAULTS = {
+			time : 7000,
+			width : 900,
+			height : 400,
+			bg : '#ffffff',
+			border : '1px solid #ffffff',
+			callback : function() {
+			}
+		};
+	}
+
+	function setImages(element, jsonArray) {
+		for( i = 0; i < jsonArray.length; i++) {
+			var image = jsonArray[i];
+			//ul.html(ul.html()+"<li id="+"slide-"+i+"><img class=\"slides\" src="+image.url+ "></img></li>")
+			element.append("<li id=" + "slide-" + i + "><img class=\"slides\" src=" + image.url + "></img></li>");
+			element.find("img:last").height(OPTIONS.height).width(OPTIONS.width);
+			LIST_ARRAY[i]=element.find("li:last");
+		}
+	}
+
+
+	$.fn.squaresMask = function(method) {
+		if(methods[method]) {
+			methods[method].apply(Array.prototype.slice.call(arguments, 1))
+		} else if( typeof method === 'object' || !method) {
+			methods['init'].apply(this, arguments)
+		} else {
+			console.log('method ' + method + 'does not exists in squaresMask')
+		}
+	}
 })(jQuery);
 
